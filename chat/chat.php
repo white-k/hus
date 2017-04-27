b5242f88d6c7594c77826c0f7d2dab24a1d09bf4
white-k <user-zjiong@outlook.com>
2017-04-27 12:52:08 +0800

    Update chat.php

--- chat/chat.php
+++ chat/chat.php
@@ -1,11 +1,23 @@
 <?php
 	include_onece('init_inc.php');
-	session_start();
+	$id=$_POST['id'];
+	$arr=array();
+	if(!get_magic_quote_gpc())
+	{
+		$id=addslashes($id);
+	}
 	$db=mysqli_connect(HOST,USER,A,DB);
 	if(mysqli_connect_errno())
 	{
 		echo "not connect database.";
 	}
-	$query="select * from message where customer='".$_SESSION['user']."'";
+	$query="select * from message where customerId>".$id;
+	$result=$db->query($query);
+	while($row=$result->fetch_assoc())
+	{
+		array_push($arr,$row);
+		
+	}
+	echo json_encode($arr);
 
 ?>
