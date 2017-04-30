9233f0f8c97e524a72ad4b50dd4b5203cccc95d7
white-k <user-zjiong@outlook.com>
2017-04-30 21:54:37 +0800

    Update chatSave.php

--- chat/chatSave.php
+++ chat/chatSave.php
@@ -1,6 +1,5 @@
 <?php
 include('init_inc.php');
-	session_start();
 	
 	$db=mysqli_connect(HOST,USER,A,DB);
 	if(mysqli_connect_errno())
@@ -8,52 +7,22 @@ include('init_inc.php');
 		echo "the database not connect";
 	}
 	
-	$oldCustomer=$_SESSION['user'];
+	
 	$mes=$_POST['name'];
-	$_SESSION['user']=$mes;
+	
 	$value=$_POST['value'];
 	
-	if($mes==$oldCustomer)
-	{	
-		$query="insert into message(customer,message) values('".$mes."','".$value."')";
-		$result=$db->query($query);
 	
-		if(!$result)
-		{
-			echo "not connect data";
-		}else{
-			echo "sucess postMessage";
-		}
+	
+	$query="insert into message(customer,message,date) values('".$mes."','".$value."',NOW())";
+	$result=$db->query($query);
+
+	if(!$result)
+	{
+		echo "not connect data";
 	}else{
-		$query="truncate table message";
-		$result=$db->query($query);
-		if(!$result)
-		{
-			echo "not connect data";
-		}else{
-			echo "sucess postMessage";
-		}
-		$query="insert into message(customer,message) values('".$mes."','".$value."')";
-		$result=$db->query($query);
-		if(!$result)
-		{
-			echo "not connect data";
-		}else{
-			echo "sucess postMessage";
-		}
-	
+		echo "sucess postMessage";
 	}
-
-	
-
-
-
-
-
-
-
-
-
 
 
 
